8a69076b725aa727f7c809d8e39b3d5b61eb328d
gringrape <53764714+gringrape@users.noreply.github.com>
2020-11-20 22:48:21 +0900

    Implement MainPage
    renders button and title
    button will get user to MakeSentencePage

---
+++ src/MainPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function MainPage() {
+  return (
+    <div>
+      <h1>
+        문장 만들기
+      </h1>
+      <button type="button">
+        시작하기
+      </button>
+    </div>
+  );
+}
