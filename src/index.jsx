285b94cec3966baa97c6f25c5e4983dce5081a7c
gringrape <53764714+gringrape@users.noreply.github.com>
2020-11-22 13:22:28 +0900

    Fix deploy problem

--- src/index.jsx
+++ src/index.jsx
@@ -11,7 +11,7 @@ import App from './App';
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </Provider>,
