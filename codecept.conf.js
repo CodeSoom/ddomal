334731c0b1b57a5cc3205f548d64b46fe263657c
gringrape <53764714+gringrape@users.noreply.github.com>
2020-11-27 06:08:43 +0900

    Set HEADLESS option to codeceptjs
    To run e2e test in the middle of CI process

--- codecept.conf.js
+++ codecept.conf.js
@@ -1,3 +1,9 @@
+const { setHeadlessWhen } = require('@codeceptjs/configure');
+
+// turn on headless mode when running with HEADLESS=true environment variable
+// HEADLESS=true npx codecept run
+setHeadlessWhen(process.env.HEADLESS);
+
 exports.config = {
   tests: './*_test.js',
   output: './output',
