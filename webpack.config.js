8aa938b965d9a562b7f1e088c5de39e95cf5722d
gringrape <53764714+gringrape@users.noreply.github.com>
2020-11-27 22:54:47 +0900

    Add correct sound when sentence contains prompt

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,13 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.mp3$/,
+        loader: 'file-loader',
+        query: {
+          name: 'static/media/[name].[hash:8].[ext]',
+        },
+      },
     ],
   },
   resolve: {
