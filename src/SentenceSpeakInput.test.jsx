c5d3bb13004197bf1730f0df41b1ba02585e5dcd
gringrape <53764714+gringrape@users.noreply.github.com>
2020-11-28 07:46:59 +0900

    Let user know prompt is not in the sentence

--- src/SentenceSpeakInput.test.jsx
+++ src/SentenceSpeakInput.test.jsx
@@ -9,6 +9,7 @@ import MicState from './enums/MicState';
 describe('SentenceSpeakInput', () => {
   const spokenSentence = '사과가 맛있네요';
   const micButton = 'mic';
+  const warningMessage = '제시어를 사용해서 문장을 말해보세요~';
 
   const handleClick = jest.fn();
 
@@ -47,24 +48,48 @@ describe('SentenceSpeakInput', () => {
   });
 
   context('when spoken sentence contains prompt', () => {
+    const prompt = '사과';
+    const sentence = '사과는 맛있다';
+
     it('play correct sound ', () => {
       renderSentenceSpeakInput({
-        prompt: '사과',
-        sentence: '사과는 맛있다',
+        prompt,
+        sentence,
       });
 
       expect(playStub).toBeCalled();
     });
+
+    it('does not show warning message', () => {
+      const { container } = renderSentenceSpeakInput({
+        prompt,
+        sentence,
+      });
+
+      expect(container).not.toHaveTextContent(warningMessage);
+    });
   });
 
   context('when spoken sentence does not contain prompt', () => {
+    const prompt = '사과';
+    const sentence = '포도는 맛있다';
+
     it('does not play correct sound', () => {
       renderSentenceSpeakInput({
-        prompt: '사과',
-        sentence: '포도는 맛있다',
+        prompt,
+        sentence,
       });
 
       expect(playStub).not.toBeCalled();
+    });
+
+    it('shows warning message', () => {
+      const { container } = renderSentenceSpeakInput({
+        prompt,
+        sentence,
+      });
+
+      expect(container).toHaveTextContent(warningMessage);
     });
   });
 });
