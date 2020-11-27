173fa47cf10d74023fc1f343d49dd903a538c9d5
gringrape <53764714+gringrape@users.noreply.github.com>
2020-11-27 11:55:52 +0900

    Extract SpokenSentence from SpeakSentenceForm

--- src/SpeakSentenceForm.jsx
+++ src/SpeakSentenceForm.jsx
@@ -5,6 +5,7 @@ import { MdMic } from 'react-icons/md';
 import styled from '@emotion/styled';
 
 import { flexBoxCenter } from './styles/common';
+import SpokenSentence from './SpokenSentence';
 
 const Container = styled.div({
   ...flexBoxCenter,
@@ -15,24 +16,16 @@ const SentenceBox = styled.div({
   padding: '12vh 0',
 });
 
-const Sentence = styled.p({
-  fontSize: '2rem',
-});
-
 const StyledMic = styled(MdMic)`
   cursor: pointer;
   color: ${({ speaking }) => (speaking ? 'green' : 'black')};
 `;
 
 export default function SpeakSentenceForm({ spokenSentence, speakStatus, onClick }) {
-  const sentence = spokenSentence ?? '문장을 소리내어 말해보세요';
-
   return (
     <Container>
       <SentenceBox>
-        <Sentence>
-          {speakStatus === 'MIC_ON' || speakStatus === 'SPEAKING' ? '...' : sentence}
-        </Sentence>
+        <SpokenSentence spokenSentence={spokenSentence} speakStatus={speakStatus} />
       </SentenceBox>
       <StyledMic
         title="mic"
