813bf501d80a22042401752e171ef72ce94a7134
gringrape <53764714+gringrape@users.noreply.github.com>
2020-12-01 11:53:46 +0900

    Fix component name

--- src/YesnoPage.jsx
+++ src/YesnoPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 
-export default function App() {
+export default function YesNoPage() {
   const dispatch = useDispatch();
 
   const handleClick = () => {
