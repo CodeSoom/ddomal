31e20febed48a0455eb9fb5ac91a5f1699e9b1f8
gringrape <53764714+gringrape@users.noreply.github.com>
2020-12-09 10:00:06 +0900

    Show yes no question answers
    Show question, correct answer and user answer

--- src/pages/YesNoAnswersPage.jsx
+++ src/pages/YesNoAnswersPage.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import YesNoAnswersContainer from '../containers/YesNoAnswersContainer';
+
 export default function YesNoPage() {
   return (
     <div>
       <h1>결과확인</h1>
+      <YesNoAnswersContainer />
     </div>
   );
 }
--- src/pages/YesNoAnswersPage.test.jsx
+++ src/pages/YesNoAnswersPage.test.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 
+import { useSelector } from 'react-redux';
+
 import { render } from '@testing-library/react';
 
 import YesNoAnswersPage from './YesNoAnswersPage';
 
+jest.mock('react-redux');
+
 describe('YesNoPage', () => {
+  beforeEach(() => {
+    useSelector.mockImplementation((selector) => selector({
+      answers: [],
+    }));
+  });
+
   it('gets next question on mount', () => {
     const { container } = render(<YesNoAnswersPage />);
 
