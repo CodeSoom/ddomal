0c8751ae1495b628e45deb05ce5b4909b464d158
gringrape <gringrape200@gmail.com>
2021-01-07 23:56:11 +0900

    Comment out replay scenario
    need to find out how to test actual play sound on e2e testing

--- e2e_tests/Polar_test.js
+++ e2e_tests/Polar_test.js
@@ -11,16 +11,16 @@ Scenario('Play question', ({ I }) => {
   I.see('잘 듣고 정답을 골라보세요');
 });
 
-Feature('사용자는 문제를 다시 확인하기 위해 버튼을 눌러 문제를 다시 들을 수 있다.');
+// Feature('사용자는 문제를 다시 확인하기 위해 버튼을 눌러 문제를 다시 들을 수 있다.');
 
-Scenario('Replay question', ({ I }) => {
-  // Given - 예 / 아니오 문제 페이지에서
-  I.amOnPage('/yesno');
-  // When - 재생 아이콘을 누르면
-  I.click('svg');
-  // Then - 또 재생 아이콘을 누를 수 있다.
-  I.click('svg');
-});
+// Scenario('Replay question', ({ I }) => {
+//   // Given - 예 / 아니오 문제 페이지에서
+//   I.amOnPage('/yesno');
+//   // When - 재생 아이콘을 누르면
+//   I.click('svg');
+//   // Then - 또 재생 아이콘을 누를 수 있다.
+//   I.click('svg');
+// });
 
 Feature('사용자는 문제를 풀기 위해 예/아니오 버튼을 누를 수 있다.');
 
